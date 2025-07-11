d6afe9d9a3c8be76af858344fb28f9abdd48fc87
chrystallag <georgiou.chrystalla.2526@gmail.com>
2025-07-11 14:47:30 -0400

    Update load_pm25_data.R

--- R/load_pm25_data.R
+++ R/load_pm25_data.R
@@ -1,9 +1,23 @@
 #' Load and Clean PM2.5 Data
 #'
+#' This function reads and cleans PM2.5 air quality data from a CSV file
+#' bundled with the package. It filters out invalid city entries and ensures
+#' cities have multiple observations and time points.
+#'
 #' @return A data frame of cleaned PM2.5 observations.
+#' @examples
+#' \dontrun{
+#'   pm25_data <- load_pm25_data()
+#' }
 #' @export
 load_pm25_data <- function() {
-  data <- read.csv(system.file("extdata", "openaq.csv", package = "appforair"), sep = ";", stringsAsFactors = FALSE)
+  csv_path <- system.file("extdata", "openaq.csv", package = "appforair")
+
+  if (csv_path == "") {
+    stop("File 'openaq.csv' not found. Make sure it exists in inst/extdata/ before building the package.")
+  }
+
+  data <- read.csv(csv_path, sep = ";", stringsAsFactors = FALSE)
 
   coords <- strsplit(data$Coordinates, ",")
   data$Latitude <- sapply(coords, function(x) as.numeric(x[1]))
